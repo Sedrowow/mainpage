a85c24b72ee65c5bba5fd180969e89b7b4e428df
Sedrowow <sedrickfanderl8@gmail.com>
2023-02-01 19:30:10 +0100

    update and test

---
+++ js/typeitcfg.js
@@ -0,0 +1,9 @@
+// Template
+new TypeIt("#ID", {
+    strings: "This will be typed!",
+}).go();
+
+new TypeIt("#troll", {
+    strings: ["You have been trolled have been trolled apeofnjopeadnjfpioawnpiolwsenpio"]
+    waitUntilVisible: true
+}).go();

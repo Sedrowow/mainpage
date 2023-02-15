731373986a8605effd4db6cc69b79183d3510b41
Sedrowow Chaos <>
2023-02-15 08:07:41 +0000

    Mainpages upgrades

--- js/typeitcfg.js
+++ js/typeitcfg.js
@@ -2,9 +2,11 @@ import './TypeIt.js';
 
 // Template
 new TypeIt("#ID", {
-    strings: "This will be typed!",
-}).go();
+	loop: true
+}).type("Sedrowow ").pause(1500).type("Chaos ").pause(1500).type("Companies").move(-16).delete(7).move(6).delete(4).move(-1).delete(1).type(".").move(2).delete(1).type(".").move(9).pause(4000).delete().go();
 
 new TypeIt("#troll", {
-    strings: ["You have been trolled have been trolled apeofnjopeadnjfpioawnpiolwsenpio"]
+  strings: ["click the button below to see our services"]
 }).go();
+
+//Sedrowow Chaos Companies
